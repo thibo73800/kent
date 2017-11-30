06e4d06ee4fa9d431ba16657d1b6eb42a6a34b44
Thibault Neveu <thibo73800@gmail.com>
2017-11-30 17:08:28 +0000

    Test

--- natural_computation/assessment_1.py
+++ natural_computation/assessment_1.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+# Code on my github:
+# https://github.com/thibo73800/kent/blob/master/natural_computation/assessment_1.py
+
+
 # I used python and Numpy which is a python library to handle matrix and vector operations
 import numpy as np
 import random
---
+++ test.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    """
+        Main function
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
