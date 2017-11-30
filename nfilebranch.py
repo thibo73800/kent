dbc3c6180e3a0ede15cc7ef68a82a849b210f2b5
Thibault Neveu <thibo73800@gmail.com>
2017-11-30 18:27:53 +0000

    addFIleInBranche

---
+++ nfilebranch.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    """
+        Main function
+    """
+    print("Hello, this is a main file")
+    a = a + 1
+    b = b
+    c = c
+    return a
+
+
+if __name__ == '__main__':
+    main()
